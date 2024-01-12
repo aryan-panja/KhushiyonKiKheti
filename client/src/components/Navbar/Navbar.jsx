4c4a9afd62e43efdbf24a5fb19e190bae5757fbe
Aryan Panja <apanja_be22@thapar.edu>
2024-01-12 22:30:34 +0530

    chatbot integrated

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="productlisting">Products</Link>
+        </li>
+        <li>
+          <Link to="/cart">Shopping Cart</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="sign-up">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/chatbot">Talk With AI</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ client/src/pages/LLM/Llm.jsx
@@ -0,0 +1,54 @@
+// ChatComponent.jsx
+import React, { useState } from 'react';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import './index.css';  // Import the ChatbotComponent.css
+import { initialChatHistory } from './history';
+import {api} from './api_key';
+
+const ChatComponent = () => {
+  const [userInput, setUserInput] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSendMessage = async () => {
+    const API_KEY = api; // Replace with your actual API key
+
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const chat = model.startChat({
+      history: initialChatHistory,
+      generationConfig: {
+        maxOutputTokens: 100,
+      },
+    });
+
+    const result = await chat.sendMessage(userInput + " and i also live in jalandhar ");
+    const responseText = await result.response.text();
+    setResponse(responseText);
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <label htmlFor="userInput">Your message:</label>
+        <input
+          type="text"
+          id="userInput"
+          placeholder="Type your message..."
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Submit</button>
+      </div>
+
+      <div>
+        <p>
+          <strong>Response:</strong>
+        </p>
+        <p className='model'>{response}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatComponent;
