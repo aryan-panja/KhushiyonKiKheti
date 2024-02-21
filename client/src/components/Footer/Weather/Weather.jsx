ed972fe01f4cca3c1399d922f054b161a853c98d
Ank206 <akapoor1_be22@thapar.edu>
2024-02-22 00:32:52 +0530

    HTTPS

--- client/src/components/Footer/Weather/Weather.jsx
+++ client/src/components/Footer/Weather/Weather.jsx
@@ -1,8 +1,8 @@
 // Weather.js
-import React, { useContext, useEffect, useState } from 'react';
-import useUserContext from '../../../Hooks/useUserContext';
+import React, { useContext, useEffect, useState } from "react";
+import useUserContext from "../../../Hooks/useUserContext";
 
-const weatherApiKey = import.meta.env.VITE_WEATHER_API
+const weatherApiKey = import.meta.env.VITE_WEATHER_API;
 
 const Weather = () => {
   const { location } = useUserContext();
@@ -11,35 +11,34 @@ const Weather = () => {
 
   // console.log(weatherApiKey)
 
-
   useEffect(() => {
     const fetchWeather = async () => {
       if (!location || !location.city || !location.state || !location.country) {
-        console.error('Invalid or undefined location data.');
-        return; 
+        console.error("Invalid or undefined location data.");
+        return;
       }
-      
+
       try {
         // Construct the API request URL
-        const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location.city},${location.state},${location.country}&appid=${weatherApiKey}`;
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location.city},${location.state},${location.country}&appid=${weatherApiKey}`;
         console.log(apiUrl);
 
         // Fetch weather information
         const weatherResponse = await fetch(apiUrl);
 
         if (!weatherResponse.ok) {
-          throw new Error('Failed to fetch weather information');
+          throw new Error("Failed to fetch weather information");
         }
 
         const weatherData = await weatherResponse.json();
         console.log("Weather.jsx : ", weatherData); // Log the weather data for debugging
-        dispatch({ type: "setWeatherData", payload: weatherData })
+        dispatch({ type: "setWeatherData", payload: weatherData });
         const temperatureInKelvin = weatherData.main.temp; // Temperature in Kelvin
         const temperatureInCelsius = temperatureInKelvin - 273.15; // Convert to Celsius
         setTemperature(temperatureInCelsius);
-        dispatch({ type: "setTemperature", payload: temperatureInCelsius })
+        dispatch({ type: "setTemperature", payload: temperatureInCelsius });
       } catch (error) {
-        console.error('Error fetching weather:', error.message);
+        console.error("Error fetching weather:", error.message);
       }
     };
 
@@ -48,7 +47,8 @@ const Weather = () => {
 
   return (
     <p>
-      , Temperature: {temperature !== null ? `${temperature.toFixed(2)} °C` : 'Loading...'}
+      , Temperature:{" "}
+      {temperature !== null ? `${temperature.toFixed(2)} °C` : "Loading..."}
     </p>
   );
 };
